a92a2dbb6b1d47438a8cbd14dc3353ce045e36d6
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-04 19:41:13 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,12 @@
+def instantiate_new_array 
+my_array = []
+end 
+
+def array_with_two_elements 
+  my_array = [1, 2] 
+end
+
+def my_first_element() 
+ my_first_element = [1, 2, 3] 
+ my_first_element[1] 
+end
