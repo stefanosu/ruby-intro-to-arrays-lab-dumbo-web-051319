584101efdd39106e8ff0bde34f2a6d9bf4021593
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-04 20:50:03 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,41 @@
+def instantiate_new_array 
+my_array = []
+end 
+
+def array_with_two_elements 
+  my_array = [1, 2] 
+end
+
+def first_element(my_first_element) 
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_first_element = @taylor_swift[0]
+end
+
+
+def third_element(my_third_element)
+  my_third_element = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_third_element[2]
+end
+
+
+def last_element(my_last_element)
+  my_last_element = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_last_element[-1]
+end 
+
+
+def first_element_with_array_methods(my_first_element_with_array_methods) 
+    my_first_element_with_array_methods = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+    my_first_element_with_array_methods.first 
+end 
+
+def last_element_with_array_methods(my_last_element_with_array_methods)
+    my_last_element_with_array_methods = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+    my_last_element_with_array_methods.last 
+end
+
+def length_of_array(my_length_of_array)
+ my_length_of_array = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  my_length_of_array.length 
+end 
+
