a65f976224755bb6290039488f755257d24fc921
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-04 20:01:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,8 +9,8 @@ end
 def my_first_element(my_first_element) 
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_first_element = @taylor_swift[0]
-  
 end
+
 
 def third_element(my_third_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
