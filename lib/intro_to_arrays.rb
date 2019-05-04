e2d66678e5190f7044eed9b96ecbbfb97cd26f35
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-04 20:05:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -13,8 +13,8 @@ end
 
 
 def third_element(my_third_element)
-  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  my_third_element = @taylor_swift[2]
+  my_third_element = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_third_element[2]
 end
 
 
