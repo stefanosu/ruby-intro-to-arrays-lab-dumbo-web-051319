c234679c4f510614e05f95dd04db38bdf0d5ecae
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-04 19:58:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -12,6 +12,7 @@ def my_first_element(my_first_element)
   
 end
 
-def third_element 
-  
+def third_element(my_third_element)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_third_element = @taylor_swift[2]
 end
