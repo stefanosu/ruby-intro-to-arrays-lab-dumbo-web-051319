d678c4277c4f57b00fd566f020baffb24d0ed075
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-04 19:59:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,7 +8,7 @@ end
 
 def my_first_element(my_first_element) 
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  my_first_element = @taylor_swift[1]
+  my_first_element = @taylor_swift[0]
   
 end
 
