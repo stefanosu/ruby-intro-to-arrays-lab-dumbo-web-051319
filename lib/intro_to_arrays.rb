dc3e78970421c535263d568c89d3f0576918614d
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-04 20:01:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,7 +6,7 @@ def array_with_two_elements
   my_array = [1, 2] 
 end
 
-def my_first_element(my_first_element) 
+def first_element(my_first_element) 
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_first_element = @taylor_swift[0]
 end
